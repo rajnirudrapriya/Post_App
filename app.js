428403cfd1a9fc05033cc6638f98d6d5a7fb7bdf
Rajni Rudrapriya <rajnirudrapriya@gmail.com>
2025-06-29 00:14:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config(); // Load variables from .env
+dotenv.config();
 
 const app = express();
 const { connectDb } = require('./config/db');
+
 const postRouter = require('./Router/postRouter');
 const userRouter = require('./Router/userRouter');
-
 
 let PORT = process.env.PORT || 5000;
 
@@ -15,14 +15,13 @@ connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/postRoute', postRoute);
-app.use('/userRoute', userRoute);
+app.use('/postRoute', postRouter);
+app.use('/userRoute', userRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello Express');
 });
 
-app.listen(PORT, err => {
-    if (err) throw err;
-    console.log(`Server is running on ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
