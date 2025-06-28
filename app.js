99057061762474e18a9b1fc459a3bb2f13b1489d
Rajni Rudrapriya <rajnirudrapriya@gmail.com>
2025-06-29 00:17:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
-dotenv.config();
-
-const app = express();
 const { connectDb } = require('./config/db');
-
 const postRouter = require('./Router/postRouter');
 const userRouter = require('./Router/userRouter');
 
-let PORT = process.env.PORT || 5000;
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
 connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Mount routers
 app.use('/postRoute', postRouter);
 app.use('/userRoute', userRouter);
 
+// Home route
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    res.send('Hello from Express on Render!');
 });
 
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
