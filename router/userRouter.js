92452c0dbc2b5ed6a78ba8e19d3bd523c1515705
Rajni Rudrapriya <rajnirudrapriya@gmail.com>
2025-06-29 00:33:02 +0530

    Delete router directory

--- router/userRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { createUser, login } = require('../controller/userController');
-const router = Router();
-
-router.post('/user', createUser);
-router.post('/users', login);
-
-module.exports = router;
